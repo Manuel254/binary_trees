8b178a34857d996b2523e6dd929c6ed99fa0c12d
Manuel254 <immanuelkituku@gmail.com>
2023-02-28 01:54:27 -0800

    Completed

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,7 +11,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *tempNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-	
+
 	if (tempNode == NULL)
 		return (NULL);
 
