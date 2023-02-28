afae5e1e9fd02ffc26d13a7a74996e173cfaba5b
Manuel254 <immanuelkituku@gmail.com>
2023-02-28 03:42:40 -0800

    Completed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,20 +10,17 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *current = NULL;
 	binary_tree_t *newNode = binary_tree_node(parent, value);
 
 	if (parent == NULL || newNode == NULL)
 		return (NULL);
 
-	if (parent->left == NULL)
-		parent->left = newNode;
-	else
+	if (parent->left != NULL)
 	{
-		current = parent->left;
-		parent->left = newNode;
-		newNode->left = current;
-		current->parent = newNode;
+		newNode->left = parent->left;
+		parent->left->parent = newNode;
 	}
+	parent->left = newNode;
+
 	return (newNode);
 }
