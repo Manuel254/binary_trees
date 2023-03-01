2f5b24253bf912f97580e3c9b4d7182bbc7a48f4
Manuel254 <immanuelkituku@gmail.com>
2023-03-01 06:42:13 -0800

    Completed

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,7 +11,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	size_t count = 0;
 
 	if (tree == NULL || tree->parent == NULL)
-		return 0;
+		return (0);
 	count += 1;
 	count += binary_tree_depth(tree->parent);
 
