2d1ff55a7ebdbe3f8514baef68a20d317ef0392b
Manuel254 <immanuelkituku@gmail.com>
2023-03-01 04:39:01 -0800

    Completed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,8 +10,8 @@
 size_t max(size_t left, size_t right)
 {
 	if (left < right)
-		return right;
-	return left;
+		return (right);
+	return (left);
 }
 
 /**
@@ -23,11 +23,12 @@ size_t max(size_t left, size_t right)
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left, right;
+
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-		return 0;
+		return (0);
 
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
-	return max(left, right) + 1;
+	return (max(left, right) + 1);
 }
